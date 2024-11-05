9032cd7e3bb87cb9d27c6e412064e12586b78a48
mosawar <94396496+mosawar@users.noreply.github.com>
2024-11-05 13:54:24 -0800

    Update AudioManager.cs

--- AudioManager.cs
+++ AudioManager.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     [Header("------- Audio Source ------")]
+    // Reference for the audio source that will play background music
     [SerializeField] AudioSource musicSource;
+    // Reference for the audio source that will play sound effects (SFX)
     [SerializeField] AudioSource SFXSource;
 
-    [Header("------- Audio Source ------")]
+    [Header("------- Audio Clips ------")]
+    // Audio clip for background music
     public AudioClip background;
+    // Audio clip for when a crow dies
     public AudioClip crowDeath;
+    // Audio clip for crow sounds
     public AudioClip crow;
+    // Audio clip for player's footsteps
     public AudioClip playerFootstep;
+    // Audio clip for sword sounds
     public AudioClip sword;
+    // Audio clip for crow attack sounds
     public AudioClip crowAttack;
 
+    // Method to play a sound effect (SFX) using the SFX audio source
+    // Takes an AudioClip as a parameter and plays it once
     public void PlaySFX(AudioClip clip)
     {
         SFXSource.PlayOneShot(clip);
