eda6b48af75d116ad06804fef7c3b4d40dabb17d
mosawar <94396496+mosawar@users.noreply.github.com>
2024-11-05 13:55:15 -0800

    Update MainMenu.cs

--- MainMenu.cs
+++ MainMenu.cs
@@ -5,24 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    // Method to start the game by loading the next scene in the build index
     public void PlayGame()
     {
-        // loads levels
+        // Loads the next scene in the build order, assuming the main game level follows the main menu
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    // Method to go to the settings menu scene
     public void GoToSettingsMenu()
     {
+        // Loads the "SettingsMenu" scene by name, which should be included in the build settings
         SceneManager.LoadScene("SettingsMenu");
     }
 
+    // Method to return to the main menu scene
     public void GoToMainMenu()
     {
+        // Loads the "MainMenu" scene by name, allowing navigation back to the main menu
         SceneManager.LoadScene("MainMenu");
     }
 
+    // Method to quit the game application
     public void QuitGame()
     {
+        // Exits the application; note that this will not function in the editor, only in a built executable
         Application.Quit();
     }
 }
